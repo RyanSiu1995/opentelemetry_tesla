299be2788498e8d98b1b44fc36bb1b642cd126c3
Ryan SIU <ryansiu1995@gmail.com>
2021-07-05 15:59:58 +0800

    Initialize the project

---
+++ lib/opentelemetry_tesla.ex
@@ -0,0 +1,18 @@
+defmodule OpentelemetryTesla do
+  @moduledoc """
+  Documentation for `OpentelemetryTesla`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpentelemetryTesla.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
